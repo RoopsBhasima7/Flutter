2c2a5edd143d3f1c82c8196acba9f25a36176fce
RoopsBhasima7 <rupeshbhasima@gmail.com>
2020-10-27 14:56:19 +0545

    Flutter App1

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Rupesh Bhasima App'),
     );
   }
 }
@@ -110,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Icon(Icons.expand_less),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
